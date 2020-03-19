eb531f3169ca83dc9885d4e3e9bcdf910bcd40bb
tyler martin <t.mar92@gmail.com>
2020-03-19 00:22:22 -0300

    adds expense form to Add and Edit components

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
+
 
 const EditExpensePage = (props) => {
   console.log(props)
   return (
-    <div>Editing the expense of item {props.match.params.id}</div>
+    <div>
+      <ExpenseForm 
+        expense={props.expense}
+        onSubmit={(expense) => {
+          console.log(expense)
+          props.dispatch(editExpense(props.match.params.id, expense));
+          props.history.push('/');
+        }}
+      />
+      <button onClick={() => {
+        props.dispatch(removeExpense({ id: props.match.params.id }));
+        props.history.push('/');
+      }}
+    >
+      Remove
+    </button>
+    </div>
   );
 }
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  }
+}
 
-export default EditExpensePage;
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
-// import RemoveExpense from './RemoveExpense';
+import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({description, amount, id, createdAt, dispatch}) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>${amount / 100} - Created At: {createdAt}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }))
-    }}>Remove</button>
+    
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
