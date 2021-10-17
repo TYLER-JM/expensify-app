0a9385deaa96d279805a0d21d339892c0e04750d
Tyler Martin <tyler.martin@electric-playground.com>
2021-10-17 14:07:01 -0300

    master updating components

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink to="/" exact={true} activeClassName="is-active">Home</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    
   </header>
 );
 export default Header;
