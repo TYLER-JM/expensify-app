68307eca5548f07406e0828adb7078a38e52983e
Tyler Martin <tyler.martin@electric-playground.com>
2021-10-17 14:34:42 -0300

    refactor EditExpensePage to class-based component, and sets up mapDispatchToProps

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,7 +5,7 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.onSubmit(expense);
+    this.props.addExpense(expense);
     this.props.history.push('/');
   }
   render() {
@@ -30,7 +30,7 @@ export class AddExpensePage extends React.Component {
 //     <ExpenseForm 
 //       onSubmit={(expense) => {
 //         // props.dispatch(addExpense(expense))
-//         props.onSubmit(expense);
+//         props.addExpense(expense);
 //         props.history.push('/');
 //       }}
 //     />
@@ -38,6 +38,6 @@ export class AddExpensePage extends React.Component {
 // );
 
 const mapDispatchToProps = (dispatch) => ({
-  onSubmit: (expense) => dispatch(addExpense(expense))
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,33 +3,66 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-
-const EditExpensePage = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <ExpenseForm 
-        expense={props.expense}
-        onSubmit={(expense) => {
-          console.log(expense)
-          props.dispatch(editExpense(props.match.params.id, expense));
-          props.history.push('/');
-        }}
-      />
-      <button onClick={() => {
-        props.dispatch(removeExpense({ id: props.match.params.id }));
-        props.history.push('/');
-      }}
-    >
-      Remove
-    </button>
-    </div>
-  );
+export class EditExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.match.params.id, expense);
+    this.props.history.push('/');
+  }
+  onClick = () => {
+    this.props.removeExpense({ id: this.props.match.params.id });
+    this.props.history.push('/');
+  }
+  render() {
+    return (
+      <div>
+        <ExpenseForm 
+          expense={this.props.expense}
+          onSubmit={this.onSubmit}
+        />
+        <button onClick={this.onclick}>
+          Remove
+        </button>
+      </div>
+    );
+  }
 }
+
+/**
+ * this stateless functional component commented out
+ * in favour of the class-based component
+ */
+// const EditExpensePage = (props) => {
+//   return (
+//     <div>
+//       <ExpenseForm 
+//         expense={props.expense}
+//         onSubmit={(expense) => {
+//           // props.dispatch(editExpense(props.match.params.id, expense));
+//           props.editExpense(props.match.params.id, expense);
+//           props.history.push('/');
+//         }}
+//       />
+//       <button onClick={() => {
+//         // props.dispatch(removeExpense({ id: props.match.params.id }));
+//         props.removeExpense({ id: props.match.params.id });
+//         props.history.push('/');
+//       }}
+//     >
+//       Remove
+//     </button>
+//     </div>
+//   );
+// }
+
 const mapStateToProps = (state, props) => {
   return {
     expense: state.expenses.find((expense) => expense.id === props.match.params.id)
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: (expense) => dispatch(removeExpense(expense))
+})
 
-export default connect(mapStateToProps)(EditExpensePage);
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
